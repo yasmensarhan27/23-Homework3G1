233edb914fbe08e709c1d087ce652726a2ff1e7a
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-10-05 15:01:54 -0400

    Update Period.py

--- Period.py
+++ Period.py
@@ -1,19 +1,20 @@
 import algorithm
-  lengths_list = input("Enter a list of length, separated by commas: ")
+lengths_list = input("Enter a list of length, separated by commas: ")
   # Split the user input string into a list.
-  lengths_list = lengths_list.split(",")
+lengths_list = lengths_list.split(",")
   # Convert the elements of the list to integers
-  integer_list = []
-  for element in lengths_list:
-    integer_list.append(int(element))
+integer_list = []
+for element in lengths_list:
+  integer_list.append(int(element))
     # Print the integer list.
-    print("your list of lengths is: ", integer_list)
+print("your list of lengths is: ", integer_list)
     #--------------------------------------------------------------#
     # Use map to apply the lambda function to each length in the list
-    periodic_times = map(calculate_periodic_time(integer_list), integer_list)
+periodic_times = map(calculate_periodic_time(integer_list), integer_list)
     # Print the periodic times and round them to 2 sig digits
-    T=[periodic_time for periodic_time in periodic_times]
-    round_time=[round(element,2) for element in T]
-    print("periodic times corresporing to the lengths are ", round_time)
-    __all__=["period"]
+T=[periodic_time for periodic_time in periodic_times]
+round_time=[round(element,2) for element in T]
+print("periodic times corresporing to the lengths are ", round_time)
+
+__all__=["period"]
     
